5d75b81ea5987b89604dd8c33995bc29102b9f41
Yan Vugenfirer <yan@daynix.com>
2018-05-31 02:26:20 +0300

    Convert test code to MGit class skeleton
    Signed-off-by: Yan Vugenfirer <yan@daynix.com>

--- mgit.rb
+++ mgit.rb
@@ -1,23 +1,51 @@
 require 'yaml'
 
-MGIT_CONFIGURATION_ENV = 'MGIT_CONFIGURATION'.freeze
+# MGit - multiple git repositories manager
+class MGit
+  def initialize
+    @config_file = ENV[MGIT_CONFIGURATION_ENV]
+  end
 
-def configuration_defined?
-  if ENV[MGIT_CONFIGURATION_ENV].nil? || ENV[MGIT_CONFIGURATION_ENV].empty?
-    false
-  else
-    true
+  def dump_config
+    config = read_config
+    puts 'Dumping configuration:'
+    puts
+    puts "\tProject directory: #{config['project_directory']}"
+    puts
+    puts "\tListing repositories in the project:"
+    config['repos'].each do |repo|
+      dump_repo_config(repo)
+    end
+  end
+
+  private
+
+  MGIT_CONFIGURATION_ENV = 'MGIT_CONFIGURATION'.freeze
+
+  def configuration_defined?
+    if @config_file.nil? || @config_file.empty?
+      false
+    else
+      true
+    end
+  end
+
+  def read_config
+    unless configuration_defined?
+      throw RuntimeError.new("Please define configuration file in \
+#{MGIT_CONFIGURATION_ENV} environment variable")
+    end
+    YAML.load_file(@config_file)
+  end
+
+  def dump_repo_config(repo)
+    puts "\t\t#{repo.keys.first}"
+    puts "\t\tUpstream url: #{repo['upstream']}"
+    puts "\t\tDevelopment url: #{repo['dev']}"
+    puts "\t\tGit reference: #{repo['ref']}"
+    puts
   end
 end
 
-unless configuration_defined?
-  puts "Please define configuration in #{MGIT_CONFIGURATION_ENV}"
-  exit(1)
-end
-
-config_file = ENV[MGIT_CONFIGURATION_ENV]
-
-puts "Loading configuration from #{config_file}"
-mgit_config = YAML.load_file(config_file)
-
-puts mgit_config.inspect.to_s
+mgit = MGit.new
+mgit.dump_config
